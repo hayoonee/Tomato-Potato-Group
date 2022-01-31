b12c4ada038e4ed8fe0721b1e319e58f77f257c1
Mustafa <mustafasibintay@gmail.com>
2022-01-31 21:18:30 +0100

    Improvements
    Card Drag and Drop improved
    Drop zone implemented
    Play stack implemented

--- Tomato Potato Group/Assets/CardDrag.cs
+++ Tomato Potato Group/Assets/CardDrag.cs
@@ -4,29 +4,61 @@ using UnityEngine;
 
 public class CardDrag : MonoBehaviour
 {
+    public GameObject Canvas;
+    public GameObject DropZone;
+
     private bool isDragging = false;
+    private GameObject startParent;
+    private GameObject dropZone;
+    private Vector2 startPosition;
+    private bool isOverDropZone;
 
     void Start()
     {
-        
+        Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("Drop Zone");
     }
 
     public void StartDrag()
     {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
     }
 
     public void StopDrag()
     {
         isDragging = false;
+        if(isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+            GetComponent<CardDrag>().enabled = false;
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
-    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
     void Update()
     {
         if(isDragging)
         {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
         }
     }
 }
